a0011312f5f5f8160b91c5253d13995d7daa8b3a
yoshito <yoshito5211@gmail.com>
2021-08-04 03:17:44 +0900

    会員の新規登録・編集・削除機能を追加

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -12,13 +12,41 @@ class MembersController < ApplicationController
     @member = Member.find(params[:id])
   end
 
-  def new; end
+  def new
+    @member = Member.new(birthday: Date.new(1980, 1, 1))
+  end
 
-  def create; end
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      redirect_to @member, notice: '会員登録しました。'
+    else
+      render :new
+    end
+  end
 
-  def edit; end
+  def edit
+    @member = Member.find(params[:id])
+  end
 
-  def update; end
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to @member, notice: '会員情報を更新しました。'
+    else
+      render :edit
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @member = Member.find(params[:id])
+    @member.destroy!
+    redirect_to members_path, notice: '会員を削除しました。'
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:number, :name, :full_name, :sex, :birthday, :email, :admin)
+  end
 end
