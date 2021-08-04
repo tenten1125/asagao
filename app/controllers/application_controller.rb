e014c212aefeb18430c3a5b217669e458c1db1c9
yoshito <yoshito5211@gmail.com>
2021-08-04 12:42:34 +0900

    ログイン・ログアウト機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_member
+
+  private
+
+  def current_member
+    Member.find_by(id: session[:member_id]) if session[:member_id]
+  end
 end
